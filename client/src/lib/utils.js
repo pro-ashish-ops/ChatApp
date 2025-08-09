f737bcafe786a2a55d7f2d2c9c85f5ee08312480
pro-ashish-ops <pakhilesh1123@gmail.com>
2025-08-09 21:45:42 +0530

    Delete client directory

--- client/src/lib/utils.js
+++
@@ -1,8 +0,0 @@
-export function formatMessageTime(date) {
-  return new Date(date).toLocaleTimeString("en-US", {
-    timeZone: "Asia/Kolkata",
-    hour: "2-digit",
-    minute: "2-digit",
-    hour12: false,
-  });
-}
